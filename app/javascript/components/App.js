635663fec9bc8c03bcc6bf57d977712dd2e8ab19
John McBride <john@alternatelabs.co>
2018-12-11 19:00:13 +0000

    add logic to for sub button

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -16,13 +16,31 @@ class App extends React.Component {
       let substitute = this.state.substitute;
       substitute.push(player);
       this.setState({ substitute })
+    } else {
+      const substitute = this.state.substitute[0];
+      if(substitute.name === player.name) return;
+      if(substitute.type === 'GK' && player.type !== 'GK') return
+
+
+      const substituteIndex = this.state.squadList[substitute.currentGroup].findIndex((element) => {
+        return element.name === substitute.name;
+      });
+      const playerIndex = this.state.squadList[player.currentGroup].findIndex((element) => {
+        return element.name === player.name;
+      });
+      let squadList = this.state.squadList
+      squadList[substitute.currentGroup][substituteIndex] = player
+      squadList[player.currentGroup][playerIndex] = substitute
+      this.setState({
+        squadList: squadList,
+        substitute: []
+      });
     }
-    console.log(this.state.squadList[`${player.position}`])
   }
 
   render() {
     return (
-      <div class="app-container">
+      <div className="app-container">
         <Squad squadList={this.state.squadList} handleSub={this.handleSub}/>
       </div>
     )
