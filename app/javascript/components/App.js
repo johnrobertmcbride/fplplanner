4e1cba5ccc6f7b0a27d18ec50c083e50fb934884
John McBride <john@alternatelabs.co>
2018-12-13 19:34:10 +0000

    only manipulate squadList once constraints met

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -11,61 +11,74 @@ class App extends React.Component {
   }
 
   handleSub = (clickedPlayer) => {
-    console.log("Clicked:")
-    console.log(clickedPlayer.name)
-    let squadList = this.state.squadList;
-    const selectedPlayer = squadList.filter(player => player.selected)[0]
-    if(selectedPlayer) {
-      console.log("Selected:")
-      console.log(selectedPlayer.name)
-    }
-    // if no selected player
-    if(!selectedPlayer) {
-      console.log("No player selected")
-      squadList[clickedPlayer.index].selected = true;
-      this.setState({ squadList });
-      return
-    }
-    // if selectedPlayer reclicked
-    if(clickedPlayer.index === selectedPlayer.index) {
-      console.log("Selected player reclicked")
-      squadList[selectedPlayer.index].selected = false;
-      this.setState({ squadList });
-      return
-    }
-    // if both players are starting
-    if(selectedPlayer.starting && clickedPlayer.starting) {
-      console.log("Both players are starting")
-      return
-    }
-    // make the substitution
-    console.log("Make the substitution")
-    const tempStarting = selectedPlayer.starting
-    const tempBenchPosition = selectedPlayer.benchPosition
-    squadList[selectedPlayer.index].starting = clickedPlayer.starting
-    squadList[selectedPlayer.index].benchPosition = clickedPlayer.benchPosition
+    this.setState(function (prevState) {
+      let squadList = prevState.squadList;
+      const selectedPlayer = squadList.filter(player => player.selected)[0];
 
-    squadList[clickedPlayer.index].starting = tempStarting
-    squadList[clickedPlayer.index].benchPosition = tempBenchPosition
+      // if no selected player
+      if(!selectedPlayer) {
+        squadList[clickedPlayer.index].selected = true;
+        return { squadList }
+      }
 
-    squadList[selectedPlayer.index].selected = false
+      // if selectedPlayer reclicked
+      if(clickedPlayer.index === selectedPlayer.index) {
+        squadList[selectedPlayer.index].selected = false;
+        return { squadList }
+      }
 
-    // if constraints met then commit the substition
-    const newStartingPlayers = squadList.filter(player => player.starting)
-    const newStartingGKs = newStartingPlayers.filter(player => player.type === "GK").length
-    const newStartingDEFs = newStartingPlayers.filter(player => player.type === "DEF").length
-    const newStartingFWDs = newStartingPlayers.filter(player => player.type === "FWD").length
-    console.log(newStartingGKs)
-    console.log(newStartingDEFs)
-    console.log(newStartingFWDs)
+      // if both players are starting
+      if(selectedPlayer.starting && clickedPlayer.starting) {
+        return
+      }
 
-    if(newStartingGKs === 1 && newStartingDEFs > 2 && newStartingFWDs > 0) {
-      console.log("Constraints met!")
-      this.setState({ squadList })
-    } else {
-      console.log("Constraints not met!")
-    }
+      // calculate newStartingPlayers
+      const newStartingPlayers = squadList.filter(player => player.starting)
+      let newStartingGKs = newStartingPlayers.filter(player => player.type === "GK").length
+      let newStartingDEFs = newStartingPlayers.filter(player => player.type === "DEF").length
+      let newStartingFWDs = newStartingPlayers.filter(player => player.type === "FWD").length
+      if(selectedPlayer.type === "GK" && selectedPlayer.starting) {
+        newStartingGKs--
+      } else if(selectedPlayer.type ==="DEF" && selectedPlayer.starting) {
+        newStartingDEFs--
+      } else if(selectedPlayer.type === "FWD" && selectedPlayer.starting) {
+        newStartingFWDs--
+      } else if(selectedPlayer.type === "GK" && !selectedPlayer.starting) {
+        newStartingGKs++
+      } else if(selectedPlayer.type ==="DEF" && !selectedPlayer.starting) {
+        newStartingDEFs++
+      } else if(selectedPlayer.type === "FWD" && !selectedPlayer.starting) {
+        newStartingFWDs++
+      }
+      if(clickedPlayer.type === "GK" && clickedPlayer.starting) {
+        newStartingGKs--
+      } else if(clickedPlayer.type ==="DEF" && clickedPlayer.starting) {
+        newStartingDEFs--
+      } else if(clickedPlayer.type === "FWD" && clickedPlayer.starting) {
+        newStartingFWDs--
+      } else if(clickedPlayer.type === "GK" && !clickedPlayer.starting) {
+        newStartingGKs++
+      } else if(clickedPlayer.type ==="DEF" && !clickedPlayer.starting) {
+        newStartingDEFs++
+      } else if(clickedPlayer.type === "FWD" && !clickedPlayer.starting) {
+        newStartingFWDs++
+      }
 
+      // if constraints met
+      if(newStartingGKs === 1 && newStartingDEFs > 2 && newStartingFWDs > 0) {
+        // make the substitution
+        const tempStarting = selectedPlayer.starting
+        const tempBenchPosition = selectedPlayer.benchPosition
+        squadList[selectedPlayer.index].starting = clickedPlayer.starting
+        squadList[selectedPlayer.index].benchPosition = clickedPlayer.benchPosition
+
+        squadList[clickedPlayer.index].starting = tempStarting
+        squadList[clickedPlayer.index].benchPosition = tempBenchPosition
+
+        squadList[selectedPlayer.index].selected = false
+        return { squadList }
+      }
+    })
   }
 
   render() {
