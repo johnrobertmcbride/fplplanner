c2994c3ca1b8ee41cf30d0446cc4c670a013bda3
John McBride <john@alternatelabs.co>
2018-12-15 17:40:23 +0000

    refactor

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -17,7 +17,7 @@ class App extends React.Component {
 
   handleSub = (squadIndex, clickedPlayer) => {
     this.setState(function (prevState) {
-      let squadLists = prevState.squadLists
+      let squadLists = _.cloneDeep(prevState.squadLists)
       let squadList = squadLists[squadIndex]
       const selectedPlayer = squadList.filter(player => player.selected)[0];
 
@@ -38,50 +38,25 @@ class App extends React.Component {
         return
       }
 
+      // make the substitution
+      const tempStarting = selectedPlayer.starting
+      const tempBenchPosition = selectedPlayer.benchPosition
+      squadList[selectedPlayer.index].starting = clickedPlayer.starting
+      squadList[selectedPlayer.index].benchPosition = clickedPlayer.benchPosition
+
+      squadList[clickedPlayer.index].starting = tempStarting
+      squadList[clickedPlayer.index].benchPosition = tempBenchPosition
+
+      squadList[selectedPlayer.index].selected = false
+
       // calculate newStartingPlayers
       const newStartingPlayers = squadList.filter(player => player.starting)
       let newStartingGKs = newStartingPlayers.filter(player => player.type === "GK").length
       let newStartingDEFs = newStartingPlayers.filter(player => player.type === "DEF").length
       let newStartingFWDs = newStartingPlayers.filter(player => player.type === "FWD").length
-      if(selectedPlayer.type === "GK" && selectedPlayer.starting) {
-        newStartingGKs--
-      } else if(selectedPlayer.type ==="DEF" && selectedPlayer.starting) {
-        newStartingDEFs--
-      } else if(selectedPlayer.type === "FWD" && selectedPlayer.starting) {
-        newStartingFWDs--
-      } else if(selectedPlayer.type === "GK" && !selectedPlayer.starting) {
-        newStartingGKs++
-      } else if(selectedPlayer.type ==="DEF" && !selectedPlayer.starting) {
-        newStartingDEFs++
-      } else if(selectedPlayer.type === "FWD" && !selectedPlayer.starting) {
-        newStartingFWDs++
-      }
-      if(clickedPlayer.type === "GK" && clickedPlayer.starting) {
-        newStartingGKs--
-      } else if(clickedPlayer.type ==="DEF" && clickedPlayer.starting) {
-        newStartingDEFs--
-      } else if(clickedPlayer.type === "FWD" && clickedPlayer.starting) {
-        newStartingFWDs--
-      } else if(clickedPlayer.type === "GK" && !clickedPlayer.starting) {
-        newStartingGKs++
-      } else if(clickedPlayer.type ==="DEF" && !clickedPlayer.starting) {
-        newStartingDEFs++
-      } else if(clickedPlayer.type === "FWD" && !clickedPlayer.starting) {
-        newStartingFWDs++
-      }
 
       // if constraints met
       if(newStartingGKs === 1 && newStartingDEFs > 2 && newStartingFWDs > 0) {
-        // make the substitution
-        const tempStarting = selectedPlayer.starting
-        const tempBenchPosition = selectedPlayer.benchPosition
-        squadList[selectedPlayer.index].starting = clickedPlayer.starting
-        squadList[selectedPlayer.index].benchPosition = clickedPlayer.benchPosition
-
-        squadList[clickedPlayer.index].starting = tempStarting
-        squadList[clickedPlayer.index].benchPosition = tempBenchPosition
-
-        squadList[selectedPlayer.index].selected = false
         return { squadLists }
       }
     })
