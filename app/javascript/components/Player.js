a0e0f499b1cc01144beb9d3a5aeff68abdce7952
John McBride <john@alternatelabs.co>
2018-12-16 19:14:04 +0000

    add random fixtures

--- app/javascript/components/Player.js
+++ app/javascript/components/Player.js
@@ -4,16 +4,33 @@ class Player extends React.Component {
   constructor(props) {
     super(props)
   }
+  randomFixture = () => {
+    const num = Math.ceil(Math.random() * 5)
+    if(num == 1) {
+      return "-one"
+    } else if (num == 2) {
+      return "-two"
+    } else if (num == 3) {
+      return "-three"
+    } else if (num == 4) {
+      return "-four"
+    } else {
+      return "-five"
+    }
+  }
 
   render() {
     return (
       <div className="player-container">
-        <span>{this.props.player.position}</span>
-        <span>
+        <span>{this.props.player.type}: </span>
+        <span className="player-name">
           {this.props.player.name}
         </span>
         <span className={`substitute-button ${this.props.player.selected ? 'selected' : ''}`} onClick={() => this.props.handleSub(this.props.squadIndex, this.props.player)}>
           S
+        </span>
+        <span className={`fixture ${this.randomFixture()}`}>
+          FIX
         </span>
       </div>
     )
